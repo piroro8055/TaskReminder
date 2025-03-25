61cc20d0ed970cd3b2f17a9eeaf14bf2db0b8f7c
Peter Anih <peteranih8055@gmail.com>
2025-03-25 23:28:23 +0000

    progression

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,4 @@ Route::get('/tasks',[TaskController::class,'index'])->name('tasks.index');
 Route::get('/tasks/create',[TaskController::class,'create'])->name('tasks.create');
 Route::post('/tasks',[TaskController::class,'store'])->name('tasks.store');
 Route::get('/tasks/{task}',[TaskController::class,'edit'])->name('tasks.edit');
-Route::put('/tasks/task',[TaskController::class,'update'])->name('tasks.update');
+Route::put('/tasks/{task}',[TaskController::class,'update'])->name('tasks.update');
