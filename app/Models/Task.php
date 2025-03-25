4122cbae2b6f618cf73347e2669b87524fa369c1
Peter Anih <peteranih8055@gmail.com>
2025-03-25 07:51:54 +0000

    progression

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+    protected $fillable = ['title', 'description', 'priority', 'due_date', 'completed', 'completed_at'];
 }
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Other meta tags and CSS links -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Your content here -->
+    <br>
+    <br>
+    <div class="container text-center">
+        <h1>Task Management System</h1>
+
+        <h2>Task List</h2> @if(session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+        @endif
+        <!-- Rest of your content -->
+    </div>
+
+    <div class="container">
+        <a href="{{ route('tasks.create') }}" class="btn btn-primary mb-3">Remind Me</a>
+        <table class="table table-bordered">
+            <thead class="thead-dark">
+                <tr>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th>Priority</th>
+                    <th>Due Date</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                <tr>
+                    <td>{{ $task->title }}</td>
+                    <td>{{ $task->description }}</td>
+                    <td>{{ $task->priority }}</td>
+                    <td>{{ $task->due_date }}</td>
+                   
+
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
