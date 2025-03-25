7aa64e6ffe33425c503ab9dbb087d58040259b63
Peter Anih <peteranih8055@gmail.com>
2025-03-25 06:19:31 +0000

    progression

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,12 +7,10 @@ use App\Models\Task;
 class TaskController extends Controller
 {
     public function index()
-    (
+    {
         $tasks = Task::where('completed', false)->orderBy('priority','desc')->orderBy('due_date')->get();
+        return view('tasks.index',compact('tasks'));
 
-        return view('task.index',compact('tasks'));
-
-    )
-
+    }
  
 }
