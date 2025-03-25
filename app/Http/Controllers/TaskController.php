0ca8e336d086fa9671d5c482bbc01069ea83a0fe
Peter Anih <peteranih8055@gmail.com>
2025-03-25 00:55:04 +0000

    progression

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+class TaskController extends Controller
+{
+    public function index()
+    (
+        $tasks = Task::where('completed', false)->orderBy('priority','desc')->orderBy('due_date')->get();
+
+        return view('task.index',compact('tasks'));
+
+    )
+
+ 
+}
