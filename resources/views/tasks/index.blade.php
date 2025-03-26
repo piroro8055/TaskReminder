2e2327f189ea6c2478dd942fdc2eda27c7dbbcf7
Peter Anih <peteranih8055@gmail.com>
2025-03-26 00:15:12 +0000

    Add task creation and edit views

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -44,6 +44,15 @@
                         <a href="{{ route('tasks.edit', $task->id) }}" class="btn btn-primary btn-sm">
                             <i class="fa fa-edit"></i> Edit
                         </a>
+                        
+                        <form action="{{ route('tasks.destroy', $task->id) }}" method="POST" style="display: inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger btn-sm"
+                                onclick="return confirm('Are you sure you want to delete this task?')">
+                                <i class="fa fa-trash"></i> Delete
+                            </button>
+                        </form>
 
                 </td>
                    
