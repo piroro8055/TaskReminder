43dd10b6de6c91b06ccd7707019f5dbfc8289cad
Peter Anih <peteranih8055@gmail.com>
2025-03-28 17:46:45 +0000

    progression

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -12,10 +12,34 @@ class TaskController extends Controller
         return view('tasks.index',compact('tasks'));
 
     }
+
+    public function create()
+    {
+        return view('tasks.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+           'title'=>'required|max:225',
+           'description'=>'nullable',
+           'priority'=>'required|in:low,medium,high',
+           'due_date'=>'nullable|max:225',
+        ]);
+
+        Task::create([
+            'title'=>$request->input('title'),
+            'description'=>$request->input('description'),
+            'priority'=>$request->input('priority'),
+            'due_date'=>$request->input('due_date'),
+        ]);
+
+        return redirect()->route('tasks.index')->with('success','Task Created');
  
+   
     public function edit(Task $task)
     {
-        return view('tasks.edit',compact('task'));
+         return view('tasks.edit',compact('task'));
 
     }
 
@@ -39,6 +63,7 @@ class TaskController extends Controller
          return redirect()->route('tasks.index')->with('success','Task Created');
 
     }
+    
     public function destroy(Task $task)
     {
         $task->delete();
